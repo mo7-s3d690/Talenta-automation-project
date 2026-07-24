83bd27d421011e499a927aefde25f73bf3c4bae9
Dalia Hossam <daliahossameldeen12@gmail.com>
2026-07-24 18:51:26 +0300

    Update actions.py

--- Deterministic_routing/actions.py
+++ Deterministic_routing/actions.py
@@ -1,11 +1,15 @@
-def accept_candidate():
-    print("Candidate accepted")
-    print("Interview has been scheduled")
-
-def reject_candidate(): 
-    print("Candidate rejected")
-    print("Rejection email has been sent")
-
-def idle_candidate(): 
-    print("Candidate needs recruiter review")
-    print("Forward to Human Recruiter")
+def execute_action(decision):
+    decision = decision.strip().upper()
+    if decision == "ACCEPT":
+        print ("Candidate Accepted")
+        print("Action: Schedule Interview")
+    elif decision == "REJECT":
+        print("Candidate Rejected")
+        print("Action: Send Rejection email")
+    elif decision == "IDLE":
+        print("Candidate needs Human Review")
+        print("Action: Forward to Recruiter")
+    else:
+        print(f"Unkown decision{decision}")
+        
+    
